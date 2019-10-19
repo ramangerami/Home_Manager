34fda087cafc3705ace47845e4338a991b696d4e
Kevin Shi <kshi5@my.bcit.ca>
2019-10-18 19:34:50 -0700

    reded abstract_home validators

--- abstract_home.py
+++ abstract_home.py
@@ -1,9 +1,18 @@
 class AbstractHome:
     """ Representation of a home object, with relevant details to buyers/sellers """
-    def __init__(self, home_id, square_feet, year_built, rooms, bathrooms, city, seller, tax ):
+
+    HOME_ID_LABEL = "Home ID"
+    SQUARE_FOOTAGE_LABEL = "Square Footage"
+    YEAR_BUILD_LABEL = "Year Build"
+    NUM_ROOMS_LABEL = "Number of Rooms"
+    NUM_BATHROOMS_LABEL = "Number of Bathrooms"
+    CITY_LABEL = "City Name"
+    SELLING_AGENT_LABEL = "Selling Agent Name"
+    YEARLY_TAX_LABEL = "Yearly Property Tax"
+
+    def __init__(self,square_feet, year_built, rooms, bathrooms, city, seller, tax ):
         """ Constructor for an Abstract Home Object """
 
-        self._home_id = home_id
         self._square_footage = square_feet
         self._year_built = year_built
         self._number_of_rooms = rooms
@@ -11,6 +20,8 @@ class AbstractHome:
         self._city = city
         self._selling_agent = seller
         self._yearly_property_tax = tax
+
+        self._home_id = None
 
     def get_id(self):
         """ Returns id of a home object """
@@ -61,32 +72,77 @@ class AbstractHome:
         """ Returns the type of a home object """
         raise NotImplemented("Method must be implemented")
 
-    @staticmethod
-    def _validate_string(x):
+    @classmethod
+    def _validate_general_input(cls, display_name, val):
+         """ Used to validate a variable for not being None """
+         if val is None:
+             raise ValueError(display_name + " cannot be undefined.")
+
+    @classmethod
+    def _validate_string_input(cls, display_name, str_val):
         """ Used to validate a string variable """
-        if x is None or type(x) != str:
-            raise ValueError("Must be a non-empty string")
-
-    @staticmethod
-    def _validate_float(x):
+        cls._validate_general_input(display_name, str_val)
+        if type(str_val) is not str:
+            raise ValueError(display_name + " must be of type: String.")
+        if str_val == "":
+            raise ValueError(display_name + " cannot be empty string.")
+             
+    @classmethod
+    def _validate_float_input(cls, display_name, flt_val):
         """ Used to validate a float variable """
-        if x is None or type(x) != float:
-            raise ValueError("Must be a non-empty float")
+        cls._validate_general_input(display_name, flt_val)
+        if type(flt_val) is not float:
+            raise ValueError(display_name + " must be of type: Float.")
+             
+    @classmethod
+    def _validate_int_input(cls, display_name, int_val):
+        """ Used to validate a string variable """
+        cls._validate_general_input(display_name, int_val)
+        if type(int_val) is not int:
+            raise ValueError(display_name + " must be of type: Integer.")
+             
+    @classmethod
+    def _validate_bool_input(cls, display_name, bool_val):
+        """ Used to validate a string variable """
+        cls._validate_general_input(display_name, bool_val)
+        if type(bool_val) is not bool:
+            raise ValueError(display_name + " must be of type: Boolean.")
+             
+    @classmethod
+    def _validate_home_input(cls, display_name, home_val):
+        """ Used to validate a string variable """
+        cls._validate_general_input(display_name, home_val)
+        if not isinstance(type(home_val), AbstractHome):
+            raise ValueError(display_name + " must be a class that extends AbstractHome.")
+             
+    # kev - I commented your old validators and made new ones that give more detailed error messages
+    
+    # @staticmethod
+    # def _validate_string(x):
+    #     """ Used to validate a string variable """
+    #     if x is None or type(x) != str:
+    #         raise ValueError("Must be a non-empty string")
 
-    @staticmethod
-    def _validate_int(x):
-        """ Used to validate an int variable """
-        if x is None or type(x) != int:
-            raise ValueError("Must be a non-empty int")
+    # @staticmethod
+    # def _validate_float(x):
+    #     """ Used to validate a float variable """
+    #     if x is None or type(x) != float:
+    #         raise ValueError("Must be a non-empty float")
 
-    @staticmethod
-    def _validate_boolean(x):
-        """ Used to validate a boolean variable """
-        if x is None or type(x) != bool:
-            raise ValueError("Must be a valid Boolean value")
+    # @staticmethod
+    # def _validate_int(x):
+    #     """ Used to validate an int variable """
+    #     if x is None or type(x) != int:
+    #         raise ValueError("Must be a non-empty int")
 
-    @staticmethod
-    def _validate_vehicle(x):
-        """ Used to validate Vehicle objects """
-        if x is None or type(x) != AbstractHome:
-            raise ValueError("Must be a Vehicle object")
+    # @staticmethod
+    # def _validate_boolean(x):
+    #     """ Used to validate a boolean variable """
+    #     if x is None or type(x) != bool:
+    #         raise ValueError("Must be a valid Boolean value")
+
+    # @staticmethod
+    # def _validate_home(x):
+    #     """ Used to validate Vehicle objects """
+    #     if x is None or type(x) != AbstractHome:
+    #         raise ValueError("Must be a Vehicle object")
