16e135202cbab96808b148c7484e21c5458e9b00
Kevin Shi <kshi5@my.bcit.ca>
2019-10-18 22:47:26 -0700

    did a little more tests for manager

--- test_home_manager.py
+++ test_home_manager.py
@@ -2,6 +2,7 @@ import unittest
 import inspect
 from abstract_home import AbstractHome
 from detached_home import DetachedHome
+from condo import Condo
 from home_manager import HomeManager
 
 class TestCondo(unittest.TestCase):
@@ -10,12 +11,12 @@ class TestCondo(unittest.TestCase):
     def setUp(self):
         """ Creates a test fixture before each method is run """
         self.condo1 = Condo(6000, 1999, 4, 2, "Vancouver", "Adrian Gekko", 12.5, 800, False)
-        self.condo2 = Condo(7000, 2012, 4, 2, "White Rock", "Adrian Gekko", 12.5, 800, False)
-        self.condo3 = Condo(15000, 2019, 4, 2, "Surrey", "Adrian Gekko", 12.5, 800, False)
+        self.condo2 = Condo(7000, 2012, 5, 6, "White Rock", "Betty Anderson", 8.0, 120, True)
+        self.condo3 = Condo(15000, 2019, 8, 12, "Surrey", "Kyle Moignahan", 1.5, 1357, False)
 
         self.detached_home1 = DetachedHome(12000, 2010, 18, 3, "Richmond", "Camelia Stewart", 0.25, 3, True)
-        self.detached_home2 = DetachedHome(800, 1990, 18, 3, "West Vancouver", "Camelia Stewart", 0.25, 3, True)
-        self.detached_home3 = DetachedHome(9500, 2019, 18, 3, "Coquitlam", "Camelia Stewart", 0.25, 3, True)
+        self.detached_home2 = DetachedHome(800, 1990, 18, 1, "West Vancouver", "Spaghetti Alfredo", 5.0, 6, False)
+        self.detached_home3 = DetachedHome(9500, 2019, 2, 0, "Coquitlam", "John Smiff", 1.3, 10, True)
 
         self.home_manager = HomeManager()
         self.assertIsNotNone(self.home_manager, "Home Manager must be defined.")
@@ -36,3 +37,87 @@ class TestCondo(unittest.TestCase):
         """ 010A - Valid construction """
         self.assertIsNotNone(self.home_manager, "Home Manager must be defined.")
         self.assertIsInstance(self.home_manager, HomeManager, "Home Manager must be an instance of HomeManager.")
+
+    def test_add_home_valid(self):
+        """ 020A - Adding valid home objects """
+        self.assertEqual(self.home_manager.get_all_homes(), [])
+        self.home_manager.add_home(self.condo1)
+        self.assertEqual(self.home_manager.get_all_homes(), [self.condo1])
+        self.assertEqual(self.home_manager.get_home_by_id(0), self.condo1)
+        self.home_manager.add_home(self.detached_home1)
+        self.assertEqual(self.home_manager.get_home_by_id(1), self.detached_home1)
+        self.assertEqual(self.home_manager.get_all_homes(), [self.condo1, self.detached_home1])
+        self.home_manager.add_home(self.condo2)
+        self.assertEqual(self.home_manager.get_all_homes(), [self.condo1, self.detached_home1, self.condo2])
+        self.assertEqual(self.home_manager.get_home_by_id(2), self.condo2)
+
+    def test_add_home_invalid(self):
+        """ 020B - Adding invalid home objects """
+        undefined_input = None
+        # Must reject undefined parameter
+        self.assertRaisesRegex(ValueError, "Home object cannot be undefined", self.home_manager.add_home, undefined_input)
+        self.assertIsNone(self.home_manager.get_home_by_id(0))
+
+        test_string = "Hello"
+        # Must reject invalid parameter type
+        self.assertRaisesRegex(ValueError, "Home object must be a class that extends AbstractHome.", self.home_manager.add_home, test_string)
+        self.assertIsNone(self.home_manager.get_home_by_id(0))
+
+    def test_get_home_by_id_valid_is_in(self):
+        """ 030A - Getting home that is in listing with a valid id """
+        self.home_manager.add_home(self.condo1)
+        self.home_manager.add_home(self.detached_home1)
+        self.home_manager.add_home(self.condo2)
+        
+        self.assertEqual(self.home_manager.get_home_by_id(0), self.condo1)
+        self.assertEqual(self.home_manager.get_home_by_id(2), self.condo2)
+        self.assertEqual(self.home_manager.get_home_by_id(1), self.detached_home1)
+
+    def test_get_home_by_id_valid_not_in(self):
+        """ 030B - Getting home that is not in listing with a valid id """
+        self.assertIsNone(self.home_manager.get_home_by_id(0))
+        self.home_manager.add_home(self.condo1)
+        self.home_manager.add_home(self.detached_home1)
+        self.home_manager.add_home(self.condo2)
+        self.assertIsNone(self.home_manager.get_home_by_id(3))
+        self.assertIsNone(self.home_manager.get_home_by_id(-2))
+        self.assertIsNone(self.home_manager.get_home_by_id(300))
+
+    def test_get_home_by_id_invalid(self):
+        """ 030C - Getting home with invalid id """
+        self.home_manager.add_home(self.condo1)
+        self.home_manager.add_home(self.detached_home1)
+        self.home_manager.add_home(self.condo2)
+
+        undefined_input = None
+        # Must reject undefined parameter
+        self.assertRaisesRegex(ValueError, "Home ID cannot be undefined", self.home_manager.get_home_by_id, undefined_input)
+
+        test_string = "Hello"
+        # Must reject invalid parameter
+        self.assertRaisesRegex(ValueError, "Home ID must be of type: Integer", self.home_manager.get_home_by_id, test_string)
+
+
+    def test_get_all_homes_empty(self):
+        """ 040A - Getting all homes of an empty home manager """
+        all_homes = self.home_manager.get_all_homes()
+        self.assertEqual(all_homes, [])
+        self.assertEqual(len(all_homes), 0)
+
+    def test_get_all_homes_full(self):
+        """ 040B - Getting all homes of an populated home manager """
+        self.home_manager.add_home(self.condo1)
+        self.home_manager.add_home(self.condo2)
+        self.home_manager.add_home(self.detached_home1)
+
+        all_homes = self.home_manager.get_all_homes()
+        self.assertEqual(all_homes, [self.condo1, self.condo2, self.detached_home1])
+        self.assertEqual(len(all_homes), 3)
+
+    
+
+
+        
+        
+if __name__ == "__main__":
+    unittest.main()
