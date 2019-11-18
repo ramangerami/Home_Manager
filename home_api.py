e8e64a82a6a57f014d92b979a53aac9e1e26518a
Kevin Shi <kshi5@my.bcit.ca>
2019-11-17 20:11:01 -0800

    update home

--- home_api.py
+++ home_api.py
@@ -50,6 +50,44 @@ def add_home():
         )
         return response
 
+@app.route('/homemanager/homes/<int:id>', methods=['PUT'])
+def update_home(id):
+    """ Update a Home by ID """
+    content = request.json
+    if example.get_home_by_id(id) is None:
+        response = app.response_class(
+            response="Home doesn't exist",
+            status=404
+        )
+        return response
+    try:
+        if content["type"] == "Detached Home":
+            det = DetachedHome(content['square_feet'], content['year_built'], content['number_of_rooms'],
+                        content['number_of_bathrooms'], content['city'], content['selling_agent'], content['yearly_property_tax'],
+                        content['number_of_floors'], content['has_rental_suite'])
+            example.update_home(det)
+
+        elif content["type"] == "Condo":
+            con = Condo(content['square_feet'], content['year_built'], content['number_of_rooms'],
+                        content['number_of_bathrooms'], content['city'], content['selling_agent'], content['yearly_property_tax'],
+                        content['pets_allowed'], content['monthly_strata_fee'])
+            example.update_home(con)
+
+
+        else:
+            response = app.response_class(
+                response="Unsupported home type",
+                status=400
+            )
+            return response
+            
+    except ValueError as e:
+        response = app.response_class(
+            response=str(e),
+            status=400
+        )
+        return response
+    
 #works screenshots taken
 @app.route('/homemanager/homes/<int:id>', methods=['DELETE'])
 def delete_home(id):
