f103995d770a914ca5203ab8904e4a298cbad15c
Kevin Shi <kshi5@my.bcit.ca>
2019-12-04 16:36:20 -0800

    Update home_api.py
    fixed the PUT for condo

--- home_api.py
+++ home_api.py
@@ -71,7 +71,7 @@ def update_home(id):
         elif content["type"] == "condo":
             con = Condo(content['square_feet'], content['year_built'], content['number_of_rooms'],
                         content['number_of_bathrooms'], content['city'], content['selling_agent'], content['yearly_property_tax'],
-                        content['pets_allowed'], content['monthly_strata_fee'])
+                        content['monthly_strata_fee'], content['pets_allowed'])
             con.set_id(id)
             example.update_home(con)
 
